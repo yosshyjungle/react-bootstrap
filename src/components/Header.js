d4b238529da7c2dc370156aae2846f67848823b2
Tomoyuki Yoshikawa <yosshyjungle@gmail.com>
2023-10-17 17:00:37 +0900

    2nd commit

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+        <div className="logo">
+            <h3>ポートフォリオ</h3>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">ホーム</Link>
+                </li>
+                <li>
+                    <Link to="/blog">ブログ</Link>
+                </li>
+                <li>
+                    <Link to="/sns">SNS</Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+};
+
+export default Header
